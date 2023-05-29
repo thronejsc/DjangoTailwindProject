690a0c3899d544a95d374c92f575ba8bc469f496
lanskiboy <lanskiboy@gmail.com>
2023-05-29 16:56:41 +0800

    env change

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Repositories\Django Journal Publishing Management System\env
+set VIRTUAL_ENV=D:\School\4th Year - 2nd Sem\DP2\DjangoTailwindProject\env
 
 if not defined PROMPT set PROMPT=$P$G
 
