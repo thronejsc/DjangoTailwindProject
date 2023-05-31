b748a3fe80b6762d0598705b5b1e0a24a7fe8f48
Elijah Joel S. Cañarejo <ejcanarejo21@gmail.com>
2023-05-31 17:08:56 +0800

    wala
    walang binago di ko alam

--- chronicle/views.py
+++ chronicle/views.py
@@ -1,4 +1,4 @@
-from app.models import Journal, Document
+from app.models import Journal, Document, Article
 from django.shortcuts import render, get_object_or_404, redirect
 from app.forms import DocumentForm
 from django.db.models import Q
@@ -7,4 +7,38 @@ from django.db.models import Q
 
 def index(request):
     journals = Journal.objects.all()
+<<<<<<< Updated upstream
     return render(request, 'home.html')
+=======
+    return render(request, 'home.html')
+
+def journal_search(request):
+    template_name = 'journal_search.html'
+    query = request.GET.get('q')
+    schoolworks = Article.objects.all()
+    
+    """if query:
+        schoolworks = schoolworks.filter(
+            Q(description__icontains=query) 
+                       
+        )"""
+    
+    context = {
+        'schoolworks': schoolworks
+        #'query': query
+    }
+    
+    return render(request, template_name, context)
+
+def upload_document(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            document = form.save(commit=False)
+            document.save()
+            return redirect('upload_document')
+    else:
+        form = DocumentForm()
+    documents = Document.objects.all()
+    return render(request, 'upload_document.html', {'form': form, 'documents': documents})
+>>>>>>> Stashed changes
