9a0f797504179ad57fe3ace46c4224b7db5d24f3
lanskiboy <lanskiboy@gmail.com>
2023-06-08 20:55:43 +0800

    cleaned chronicles agennn

--- chronicle/views.py
+++ chronicle/views.py
@@ -7,38 +7,6 @@ from django.db.models import Q
 
 def index(request):
     journals = Journal.objects.all()
-<<<<<<< Updated upstream
-    return render(request, 'home.html')
-=======
     return render(request, 'home.html')
 
-def journal_search(request):
-    template_name = 'journal_search.html'
-    query = request.GET.get('q')
-    schoolworks = Article.objects.all()
-    
-    """if query:
-        schoolworks = schoolworks.filter(
-            Q(description__icontains=query) 
-                       
-        )"""
-    
-    context = {
-        'schoolworks': schoolworks
-        #'query': query
-    }
-    
-    return render(request, template_name, context)
 
-def upload_document(request):
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            document = form.save(commit=False)
-            document.save()
-            return redirect('upload_document')
-    else:
-        form = DocumentForm()
-    documents = Document.objects.all()
-    return render(request, 'upload_document.html', {'form': form, 'documents': documents})
->>>>>>> Stashed changes
